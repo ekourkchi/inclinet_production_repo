1295dca1c354bf12791b2bc6b40cd59677d7b9e8
ekourkchi <ekourkchi@gmail.com>
2021-09-16 00:19:50 -0700

    testing shell script

---
+++ test.sh
@@ -0,0 +1,9 @@
+python data_prep.py -p ./galaxies/ -b g -c catalogs/catalog.csv  -v
+python data_prep.py -p ./galaxies/ -b r -c catalogs/catalog.csv  -v
+python data_prep.py -p ./galaxies/ -b i -c catalogs/catalog.csv  -v
+python data_prep.py -p ./galaxies/ -b gri -c catalogs/catalog.csv  -v
+
+python data_compress.py -i 128x128_g -o compressed -s 128 -v
+python data_compress.py -i 128x128_r -o compressed -s 128 -v
+python data_compress.py -i 128x128_i -o compressed -s 128 -v
+python data_compress.py -i 128x128_RGB -o compressed -s 128 -v
